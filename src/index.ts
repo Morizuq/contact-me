ed941924d28baf929df7b02fbb7259608925d31e
Morizuq <morizuqshoneye@gmail.com>
2023-05-11 18:36:00 +0100

    patch

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const favicon = require("serve-favicon");
+import { Request, Response } from "express";
 
 require("dotenv").config();
 
@@ -11,7 +11,7 @@ const app = express();
 
 const port = process.env.PORT || 3412;
 
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.get("/favicon", (req: Request, res: Response) => res.status(204));
 
 app.use(express.json());
 
