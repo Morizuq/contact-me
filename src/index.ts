16e1bc1003099fadfcada945acda87f9c07d3fbc
Morizuq <morizuqshoneye@gmail.com>
2023-05-11 18:25:46 +0100

    serve favicon

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
 const express = require("express");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const path = require("path");
+const favicon = require("serve-favicon");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const mail = require('./router');
+const mail = require("./router");
 
 const app = express();
 
 const port = process.env.PORT || 3412;
 
-app.use(express.json())
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
-app.use('/api/v1/mail', mail)
+app.use(express.json());
 
+app.use("/api/v1/mail", mail);
 
 const start = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        app.listen(port, () => console.log(`Listening on ${port}`));
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Listening on ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //Start the server after connecting to the database
 start();
